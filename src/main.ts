38a934264aabc3ef0cce9f5a712e4edb19693bc8
Jonas Wijne <jonas.wijne@jimber.io>
2022-03-06 21:18:25 +0100

    FIX

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@polkadot/api-augment'; //see: https://github.com/polkadot-js/api/releases/tag/v7.0.1
+
 createApp(App).mount('#app')
